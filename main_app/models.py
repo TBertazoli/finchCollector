f0182cdb4983341e02a72b061e40b3550ca0d556
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2024-04-25 16:02:23 -0500

    added toy model

--- main_app/models.py
+++ main_app/models.py
@@ -11,6 +11,17 @@ MEALS = (
 )
 
 
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs={'pk': self.id})
+
+
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
