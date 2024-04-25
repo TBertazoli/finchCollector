844813244add8a6d5d44107754c205dc17b35615
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2024-04-25 16:36:46 -0500

    added toy delete and edit

--- main_app/urls.py
+++ main_app/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
          views.add_feeding, name='add_feeding'),
     path('toys/', views.ToyList.as_view(), name='toys_index'),
     path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
+    path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
+    path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
 ]
